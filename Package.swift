a86450dae57db88bd3d6c4f97d8043fd8932769a
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-08-29 22:39:36 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,23 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Toggles",
+    platforms: [ .iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Toggles",
-            targets: ["Toggles"]),
+            targets: ["Toggles"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Toggles",
             dependencies: [],
-            path: "Sources"
+            path: "Sources",
+            resources: [.process("Resources")]
         ),
         .testTarget(
             name: "TogglesTests",
