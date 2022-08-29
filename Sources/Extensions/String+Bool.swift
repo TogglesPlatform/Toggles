5b3332ce5f4281b67834868607867f2a8da5333b
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-08-29 22:35:11 +0100

    Add String+Bool.swift

---
+++ Sources/Extensions/String+Bool.swift
@@ -0,0 +1,17 @@
+//  String+Bool.swift
+
+import Foundation
+
+extension String {
+    
+    var boolValue: Bool? {
+        switch self.lowercased() {
+        case "true", "t", "yes", "y", "1":
+            return true
+        case "false", "f", "no", "n", "0":
+            return false
+        default:
+            return nil
+        }
+    }
+}
