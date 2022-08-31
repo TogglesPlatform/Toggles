4c7f38bedd5ac15c5a343d4e5ab2b818eab18fa7
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-08-31 19:29:33 +0100

    Set min iOS platform to iOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Toggles",
-    platforms: [ .iOS(.v14)],
+    platforms: [ .iOS(.v15)],
     products: [
         .library(
             name: "Toggles",
