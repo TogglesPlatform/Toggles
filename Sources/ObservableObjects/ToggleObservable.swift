4870b059e5c37b1fc5d3f932b01e485b407a923e
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-10 23:12:16 +0100

    Make ToggleObservable to publish raw values

--- Sources/ObservableObjects/ToggleObservable.swift
+++ Sources/ObservableObjects/ToggleObservable.swift
@@ -3,13 +3,28 @@
 import Combine
 import Foundation
 
-class ToggleObservable: ObservableObject {
+public class ToggleObservable: ObservableObject {
     
     var manager: ToggleManager
     var cancellables: Set<AnyCancellable> = []
     
     @Published
     var value: Value = .none
+    
+    @Published
+    var boolValue: Bool = false
+    
+    @Published
+    var intValue: Int = 0
+    
+    @Published
+    var numberValue: Double = 0.0
+    
+    @Published
+    var stringValue: String = ""
+    
+    @Published
+    var encryptedValue: String = ""
     
     init(manager: ToggleManager, variable: Variable) {
         self.manager = manager
@@ -20,6 +35,20 @@ class ToggleObservable: ObservableObject {
         manager.publisher(for: variable)
             .sink { value in
                 self.value = value
+                switch value {
+                case .none:
+                    break
+                case .bool(let value):
+                    self.boolValue = value
+                case .int(let value):
+                    self.intValue = value
+                case .number(let value):
+                    self.numberValue = value
+                case .string(let value):
+                    self.stringValue = value
+                case .encrypted(let value):
+                    self.encryptedValue = value
+                }
             }
             .store(in: &cancellables)
     }
