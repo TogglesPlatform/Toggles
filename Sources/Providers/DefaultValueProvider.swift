0fdf74fff48ea15ede54360701557ed2d22543ef
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-16 08:25:02 +0100

    Validate datasource upon loading in DefaultValueProvider

--- Sources/Providers/DefaultValueProvider.swift
+++ Sources/Providers/DefaultValueProvider.swift
@@ -4,6 +4,10 @@ import Foundation
 
 class DefaultValueProvider: ValueProvider {
     
+    private enum LoaderError: Error {
+        case foundDuplicateVariables([Variable])
+    }
+    
     public var name: String { "Default" }
     
     private let toggles: [Variable: Value]
@@ -11,7 +15,9 @@ class DefaultValueProvider: ValueProvider {
     init(jsonURL: URL) throws {
         let content = try Data(contentsOf: jsonURL)
         let datasource = try JSONDecoder().decode(Datasource.self, from: content)
-        self.toggles = Dictionary(grouping: datasource.toggles, by: \.variable)
+        let groupedToggles = Dictionary(grouping: datasource.toggles, by: \.variable)
+        try Self.validate(groupedToggles)
+        self.toggles = groupedToggles
             .mapValues { $0.first! }
             .mapValues { $0.value }
     }
@@ -22,5 +28,18 @@ class DefaultValueProvider: ValueProvider {
             assertionFailure("Found .none value for variable \(variable) in \(name) provider.")
         }
         return value
+    }
+}
+
+extension DefaultValueProvider {
+    
+    private static func validate(_ groupedToggles: [Variable: [Toggle]]) throws {
+        let duplicateVariables = groupedToggles
+            .filter { $1.count > 1 }
+            .compactMap { String($0.0) }
+            .map { String($0) }
+        if duplicateVariables.count > 0 {
+            throw LoaderError.foundDuplicateVariables(duplicateVariables)
+        }
     }
 }
