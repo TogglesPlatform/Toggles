3f7a490d92d4fe2f9040c79f95c1953830e95288
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-19 17:20:20 +0100

    Add Toggle+ComparableTests

---
+++ Tests/Suites/Extensions/Toggle+ComparableTests.swift
@@ -0,0 +1,16 @@
+//  Toggle+ComparableTests.swift
+
+import XCTest
+@testable import Toggles
+
+final class Toggle_ComparableTests: XCTestCase {
+    
+    func test_comparable() throws {
+        let toggle1 = Toggle(variable: "variable_1", value: .none, metadata: Metadata(description: "", group: ""))
+        let toggle2 = Toggle(variable: "variable_2", value: .none, metadata: Metadata(description: "", group: ""))
+        XCTAssertTrue(toggle1 == toggle1)
+        XCTAssertFalse(toggle1 == toggle2)
+        XCTAssertTrue(toggle1 < toggle2)
+        XCTAssertFalse(toggle1 > toggle2)
+    }
+}
