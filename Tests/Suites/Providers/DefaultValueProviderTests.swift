3741e0228d2150108e0bbdb5d878ad2b9f03aa66
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-19 17:21:34 +0100

    Add DefaultValueProviderTests

---
+++ Tests/Suites/Providers/DefaultValueProviderTests.swift
@@ -0,0 +1,20 @@
+//  DefaultValueProviderTests.swift
+
+import XCTest
+@testable import Toggles
+
+final class DefaultValueProviderTests: XCTestCase {
+    
+    func test_value() throws {
+        let url = Bundle.toggles.url(forResource: "TestDatasource", withExtension: "json")!
+        let provider = try DefaultValueProvider(jsonURL: url)
+        XCTAssert(provider.value(for: "integer_toggle") != .none)
+    }
+    
+    func test_duplicatesInDatasourceNotAllowed() throws {
+        let url = Bundle.toggles.url(forResource: "TestDatasourceWithDuplicateVariables", withExtension: "json")!
+        XCTAssertThrowsError(try DefaultValueProvider(jsonURL: url)) { error in
+            XCTAssertEqual(error as! TogglesValidator.LoaderError, TogglesValidator.LoaderError.foundDuplicateVariables(["boolean_toggle", "integer_toggle"]))
+        }
+    }
+}
