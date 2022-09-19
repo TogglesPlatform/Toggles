cc1670dd108684abeb60ab4cdf74169bf944a2df
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-19 17:21:50 +0100

    Add LocalValueProviderTests

---
+++ Tests/Suites/Providers/LocalValueProviderTests.swift
@@ -0,0 +1,26 @@
+//  LocalValueProviderTests.swift
+
+import XCTest
+@testable import Toggles
+
+final class LocalValueProviderTests: XCTestCase {
+    
+    func test_existingValue() throws {
+        let url = Bundle.toggles.url(forResource: "TestDatasource", withExtension: "json")!
+        let provider = try LocalValueProvider(jsonURL: url)
+        XCTAssert(provider.value(for: "integer_toggle") != .none)
+    }
+    
+    func test_missingValue() throws {
+        let url = Bundle.toggles.url(forResource: "TestDatasource", withExtension: "json")!
+        let provider = try LocalValueProvider(jsonURL: url)
+        XCTAssertEqual(provider.value(for: "non_existing_variable"), .none)
+    }
+    
+    func test_duplicatesInDatasourceNotAllowed() throws {
+        let url = Bundle.toggles.url(forResource: "TestDatasourceWithDuplicateVariables", withExtension: "json")!
+        XCTAssertThrowsError(try LocalValueProvider(jsonURL: url)) { error in
+            XCTAssertEqual(error as! TogglesValidator.LoaderError, TogglesValidator.LoaderError.foundDuplicateVariables(["boolean_toggle", "integer_toggle"]))
+        }
+    }
+}
