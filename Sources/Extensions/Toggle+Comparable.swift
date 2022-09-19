9a46bc92a9648f39ef81a78c59149096229a1d9b
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-19 17:16:33 +0100

    Split Toggle+Conformances in 2 files

--- Sources/Extensions/Toggle+Comparable.swift
+++ Sources/Extensions/Toggle+Comparable.swift
@@ -1,13 +1,10 @@
-//  Toggle+Conformances.swift
+//  Toggle+Comparable.swift
 
 import Foundation
 
 extension Toggle: Comparable {
+    
     public static func < (lhs: Toggle, rhs: Toggle) -> Bool {
         lhs.variable < rhs.variable
     }
-}
-
-extension Toggle: Identifiable {
-    public var id: String { variable }
 }
---
+++ Sources/Extensions/Toggle+Identifiable.swift
@@ -0,0 +1,7 @@
+//  Toggle+Identifiable.swift
+
+import Foundation
+
+extension Toggle: Identifiable {
+    public var id: String { variable }
+}
