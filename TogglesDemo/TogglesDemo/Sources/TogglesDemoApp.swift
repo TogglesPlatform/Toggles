0d8104cb62bcaaffa191c298e144aa49e24dbab6
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-09-25 13:22:45 +0100

    Enhance setup flow in demo app

--- TogglesDemo/TogglesDemo/Sources/TogglesDemoApp.swift
+++ TogglesDemo/TogglesDemo/Sources/TogglesDemoApp.swift
@@ -7,26 +7,50 @@ import Toggles
 @main
 struct TogglesDemoApp: App {
     
+    private enum SetupStage {
+        case start
+        case configurationSelected
+        case datasourceSelected
+    }
+    
+    @State private var setupStage: SetupStage = .start
+    @State private var setupConfiguration: DemoConfiguration.SetupConfiguration!
+    @State private var demoDatasource: DemoConfiguration.DemoDatasource!
     
     init() {}
     
     var body: some Scene {
         WindowGroup {
-            if setupConfiguration == nil {
+            switch setupStage {
+            case .start:
                 Text("")
                     .alert("Select setup", isPresented: .constant(true)) {
                         Button("Persistent") {
                             setupConfiguration = .persistent
+                            setupStage = .configurationSelected
                         }
                         Button("In Memory") {
                             setupConfiguration = .inMemory
+                            setupStage = .configurationSelected
                         }
                         Button("Immutable") {
                             setupConfiguration = .immutable
+                            setupStage = .configurationSelected
                         }
                     }
-            }
-            else {
+            case .configurationSelected:
+                Text("")
+                    .alert("Select datasource", isPresented: .constant(true)) {
+                        Button("Default") {
+                            demoDatasource = .default
+                            setupStage = .datasourceSelected
+                        }
+                        Button("Huge (10K entries)") {
+                            demoDatasource = .huge
+                            setupStage = .datasourceSelected
+                        }
+                    }
+            case .datasourceSelected:
                 DemoView(viewModel: setupDemoConfiguration())
             }
         }
