11e7f032cf67703c269cdab103492730c0cd0723
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-10-13 23:43:41 +0100

    Remove unnecessary TogglesDatasource conformance to Comparable

--- JustTweakMigrator/Sources/Models/Toggles/TogglesDatasource.swift
+++ JustTweakMigrator/Sources/Models/Toggles/TogglesDatasource.swift
@@ -5,11 +5,3 @@ import Foundation
 struct TogglesDatasource: Encodable {
     let toggles: [Toggle]
 }
-
-// move to extension
-extension TogglesDatasource: Comparable {
-    
-    static func < (lhs: TogglesDatasource, rhs: TogglesDatasource) -> Bool {
-        lhs.toggles.count < rhs.toggles.count
-    }
-}
